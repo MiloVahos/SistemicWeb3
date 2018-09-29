1e293e6e922ff10ac21bb59b0d32551c3678ba53
Raykgeneer96 <EvilGeniusesAppsCompany@gmail.com>
2018-09-29 18:11:35 -0500

    CRUD para conferences

--- src/app/components/add/add-conference/add-conference.component.ts
+++ src/app/components/add/add-conference/add-conference.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable, timer } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, share} from 'rxjs/operators';
-import { Author } from '../../../interfaces/author.interface';
 import { Conference } from '../../../interfaces/conference.interface';
 import { AuthService } from '../../../services/auth.service';
+import { AuthorsService } from '../../../services/authors.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,43 +15,34 @@ import { Router } from '@angular/router';
 })
 export class AddConferenceComponent implements OnInit {
 
-  forma: FormGroup;
+  forma: FormGroup;                   // OBJETO MODIFICADOR DEL FORMULARIO
+  showSuccess$: Observable<boolean>;  // SI SE COMPLETA UNA TRANSFERENCIA SE ACTIVA ESTA BANDERA
+  showError$: Observable<boolean>;    // SI LA TRANSFERENCIA FALLA, SE ACTIVA ESTA BANDERA
+  updateMode = false;                 // MODO UPDATE ACTIVADO
+  private updatedID: string;          // ID QUE SE VA A MODIFICAR
+  private deleteID: string;           // ID QUE SE VA A ELIMINAR
+  deleteTitle: string;                // TITULO DEL LIBRO QUE SE VA A BORRAR
 
-  showSuccess$: Observable<boolean>;
-  showError$: Observable<boolean>;
+  // VARIABLES
+  public currentYear = new Date().getFullYear();  // AÑO ACTUAL
+  closeResult: string;
 
-  public currentYear = new Date().getFullYear();
+  // VARIABLES DE LA BASE DE DATOS
+  private ConferencesCol: AngularFirestoreCollection<Conference>;
+  private ConferencesObs: Observable<Conference[]>;
+  private ConferencesList: Conference[]  = [];
+  private AuthorsNames: Array<string> = [];
 
-  conference: Object = {
-    title: '',
-    conference: '',
-    pages: '',
-    url: '',
-    ambit: '',
-    year: null,
-    authors: []
-  };
-
-  public author: any;
-  authors: Observable<any[]>;
-  authorsNames: Array<string> = [];
-
-  AuthorsColRef: AngularFirestoreCollection<Author>;
-  ConferencesColRef: AngularFirestoreCollection<Conference>;
-
-  constructor(private db: AngularFirestore,
-              private router: Router,
-              private _authS: AuthService
+  constructor(  private router: Router,
+                private _authS: AuthService,
+                private _authorsService: AuthorsService,
+                private db: AngularFirestore,
+                private modalService: NgbModal
             ) {
 
     if ( !this._authS.isUserEmailLoggedIn ) {
       this.router.navigate(['/']);
     }
-
-    this.AuthorsColRef = this.db.collection<Author>('AUTHORS');
-    this.ConferencesColRef = this.db.collection<Conference>('CONFERENCES');
-
-    this.authors = this.db.collection('AUTHORS').valueChanges();
 
     this.forma = new FormGroup({
       'title':    new FormControl( '', Validators.required ),
@@ -63,28 +55,36 @@ export class AddConferenceComponent implements OnInit {
                                             Validators.max(this.currentYear)] ),
       'authors':  new FormArray([ new FormControl('', Validators.required ) ])
     });
+
+    this.ConferencesCol = db.collection<Conference>('CONFERENCES');
+    this.ConferencesObs = this.ConferencesCol.valueChanges();
+
+  }
+
+  ngOnInit() {
+    this.AuthorsNames  = this._authorsService.getAuthorsNames();
+    this.ConferencesObs.subscribe( conferences => {
+      conferences.forEach( conference =>  {
+        this.ConferencesList.push(conference);
+      });
+    });
   }
 
   saveConference() {
 
-    const authorList: string[] = this.forma.value.authors;
-
+    const ID = this.getUniqueId();
     const conference: Conference = {
-
-      title:    this.forma.value.title,
-      conference:  this.forma.value.conference,
-      pages:    this.forma.value.pages,
-      url:      this.forma.value.url,
-      ambit:   this.forma.value.ambit,
-      year:     this.forma.value.year.toString(),
-      author:   this.forma.value.authors.join(' , ')
-
+      id:           ID,
+      title:        this.forma.value.title,
+      conference:   this.forma.value.conference,
+      pages:        this.forma.value.pages,
+      url:          this.forma.value.url,
+      ambit:        this.forma.value.ambit,
+      year:         this.forma.value.year.toString(),
+      author:       this.forma.value.authors.join(' , ')
     };
 
-    this.ConferencesColRef.add(conference).then((result) => {
-      for ( let i = 0; i < authorList.length; i++ ) {
-        this.AuthorsColRef.doc(authorList[i]).collection('CONFERENCES').add(conference);
-      }
+    this.ConferencesCol.doc(ID).set(conference).then( (result) => {
       this.showSuccess$ = timer(200).pipe( map(() => true), share() );
     })
     .catch((err) => {
@@ -105,14 +105,87 @@ export class AddConferenceComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-    this.authors.subscribe(authors => {
-      authors.forEach(author => {
-        this.authorsNames.push(author.name);
-      });
+   // UPDATE
+   updateConference ( conference: Conference ) {
+
+    this.forma.reset({
+      title:        conference.title,
+      conference:   conference.conference,
+      pages:        conference.pages,
+      url:          conference.url,
+      ambit:        conference.ambit,
+      year:         conference.year
+    });
+
+    const authors: string[] = conference.author.split(' , ');
+    for ( let i = 0; i < authors.length; i++ ) {
+      (<FormArray>this.forma.controls['authors']).push(
+        new FormControl( authors[i], Validators.required )
+      );
+    }
+    (<FormArray>this.forma.controls['authors']).removeAt(0);
+    this.updatedID  = conference.id;
+    this.updateMode = true;
+  }
+
+  update() {
+    if ( this.updateMode ) {
+      const conference: Conference = {
+        id:           this.updatedID,
+        title:        this.forma.value.title,
+        conference:   this.forma.value.conference,
+        pages:        this.forma.value.pages,
+        url:          this.forma.value.url,
+        ambit:        this.forma.value.ambit,
+        year:         this.forma.value.year.toString(),
+        author:       this.forma.value.authors.join(' , ')
+      };
+      this.ConferencesCol.doc(this.updatedID).update(conference);
+    }
+    this.forma.reset({
+      title: '',
+      conference: '',
+      pages: '',
+      url: '',
+      ambit: '',
+      year: null
+    });
+    this.forma.controls['authors'].reset();
+    while ((<FormArray>this.forma.controls['authors']).length !== 1) {
+      (<FormArray>this.forma.controls['authors']).removeAt(0);
+    }
+    this.updatedID  = '';
+    this.updateMode = false;
+  }
+
+  // DELETE
+  deleteConference ( conference: Conference, content ) {
+    this.deleteID = conference.id;
+    this.deleteTitle = conference.title;
+    this.modalService.open( content, { ariaLabelledBy: 'modal-basic-title' } )
+      .result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 
+  delete() {
+    this.ConferencesCol.doc(this.deleteID).delete();
+    this.deleteID  = '';
+    this.deleteTitle  = '';
+    this.modalService.dismissAll(this.closeResult);
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
   addAuthor() {
     (<FormArray>this.forma.controls['authors']).push(
@@ -129,8 +202,12 @@ export class AddConferenceComponent implements OnInit {
       debounceTime(200),
       distinctUntilChanged(),
       map(term => term.length < 2 ? []
-        : this.authorsNames.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+        : this.AuthorsNames.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
     )
+
+  getUniqueId() {
+    return '_' + Math.random().toString(36).substr(2, 9) + (new Date()).getTime().toString(36);
+  }
 
 }
 
