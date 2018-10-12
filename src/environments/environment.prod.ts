7f7b2bfeabbcb42409bbbf1ad407ac0102bd2541
Raykgeneer96 <EvilGeniusesAppsCompany@gmail.com>
2018-10-12 09:30:28 -0500

    Preparada la versión de producción 3.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+  apiKey: 'AIzaSyDJ7S3JhusqyovDTB_jgfaWvt1TZCyT-A4',
+    authDomain: 'sistemic-publications-prod.firebaseapp.com',
+    databaseURL: 'https://sistemic-publications-prod.firebaseio.com',
+    projectId: 'sistemic-publications-prod',
+    storageBucket: 'sistemic-publications-prod.appspot.com',
+    messagingSenderId: '174534966508'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyCybQwf1C08HhkLKoWMfOJT-Ncd8BK3Daw",
-    authDomain: "sistemic-app.firebaseapp.com",
-    databaseURL: "https://sistemic-app.firebaseio.com",
-    projectId: "sistemic-app",
-    storageBucket: "sistemic-app.appspot.com",
-    messagingSenderId: "761821678211"
+    apiKey: 'AIzaSyCybQwf1C08HhkLKoWMfOJT-Ncd8BK3Daw',
+    authDomain: 'sistemic-app.firebaseapp.com',
+    databaseURL: 'https://sistemic-app.firebaseio.com',
+    projectId: 'sistemic-app',
+    storageBucket: 'sistemic-app.appspot.com',
+    messagingSenderId: '761821678211'
   }
 
 };
